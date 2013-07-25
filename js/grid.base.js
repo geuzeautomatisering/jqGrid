08916764083f95dae58592fa752a1d8b75cbf93a
martinvd <martinvdpub@gmail.com>
2013-07-25 18:33:48 +0200

    We love bubbles

--- js/grid.base.js
+++ js/grid.base.js
@@ -2188,11 +2188,9 @@ $.fn.jqGrid = function( pin ) {
 			if($(ptr).attr("class") !== "subgrid") {
 				$(ptr).addClass("ui-state-hover");
 			}
-			return false;
 		}).bind('mouseout',function(e) {
 			ptr = $(e.target).closest("tr.jqgrow");
 			$(ptr).removeClass("ui-state-hover");
-			return false;
 		});
 		}
 		var ri,ci;
